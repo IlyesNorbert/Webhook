7131a5e86953edafa9e9a3f7dc533a492245d1b1
unknown <student@student2.napocasoftware.local>
2017-04-14 12:42:41 +0300

    pubnub private key added

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 'use strict';
 var pubnub = require("pubnub")({
     ssl           : true,  // <- enable TLS Tunneling over TCP 
-    publish_key   : "pub-c-....bd09a3eff137",
-    subscribe_key : "sub-c-.....02ee2ddab7fe"
+    publish_key   : "pub-c-b1159858-e2a5-4fda-81ba-18e5a3c7a9ed",
+    subscribe_key : "sub-c-bca32ba0-200c-11e7-bb8a-0619f8945a4f"
 });
 
 
@@ -20,7 +20,7 @@ restService.use(bodyParser.json());
 restService.post('/echo', function(req, res) {
     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.room ? req.body.result.parameters.room : "Seems like some problem. Speak again."
     pubnub.publish({ 
-                    channel   : 'my_channel',
+                    channel   : 'alma',
                     message   : initiateMessage,
                     callback  : function(e) { 
                         console.log( "SUCCESS!", e ); 
