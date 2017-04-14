3cd8a815af4333bb625203b659584cdf1c30360a
unknown <student@student2.napocasoftware.local>
2017-04-14 13:24:54 +0300

    pubnub added

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 'use strict';
-/* var pubnub = require("pubnub")({
+var pubnub = require("pubnub");
+
+pubnub = new pubnub({
     ssl           : true,  // <- enable TLS Tunneling over TCP 
     publish_key   : "pub-c-b1159858-e2a5-4fda-81ba-18e5a3c7a9ed",
     subscribe_key : "sub-c-bca32ba0-200c-11e7-bb8a-0619f8945a4f"
-}); */
+});
 
 
 const express = require('express');
@@ -19,11 +21,18 @@ restService.use(bodyParser.json());
 
 restService.post('/echo', function(req, res) {
     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.room ? req.body.result.parameters.room : "Seems like some problem. Speak again."
-    return res.json({
-        speech: "The " + speech + "  lamp is turned on",
-        displayText: "The " + speech + " lamp is turned on",
-        source: 'webhook-echo-sample'
-    });
+  	 pubnub.publish({ 
+                    channel   : 'alma',
+                    message   : speech,
+                    callback  : function(e) { 
+                        console.log( "SUCCESS!", e ); 
+					},
+                    error     : function(e) { 
+                        response.tellWithCard("Could not connect", "Drone", "Could not connect");
+                        console.log( "FAILED! RETRY PUBLISH!", e ); 
+						}
+                });  
+
 });  
 
 
