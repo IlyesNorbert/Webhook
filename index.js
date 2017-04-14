36e9886f10b91022ead0e82e003e3682d9dd5bd3
unknown <student@student2.napocasoftware.local>
2017-04-14 12:48:44 +0300

    added PubNub

--- index.js
+++ index.js
@@ -18,22 +18,13 @@ restService.use(bodyParser.urlencoded({
 restService.use(bodyParser.json());
 
 restService.post('/echo', function(req, res) {
-    var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.room ? req.body.result.parameters.room : "Seems like some problem. Speak again."
-/*     pubnub.publish({ 
-                    channel   : 'alma',
-                    message   : initiateMessage,
-                    callback  : function(e) { 
-                        console.log( "SUCCESS!", e );  */
-                        	return res.json({
-								speech: "turn on the " + speech + "lamp",
-								displayText: "turn on the " + speech + "lamp",
-								source: 'webhook-echo-sample'
-							});
-/*                         },
-                    error     : function(e) { 
-                        response.tellWithCard("Could not connect", "Drone", "Could not connect");
-                        console.log( "FAILED! RETRY PUBLISH!", e ); }
-                });  */   
+    var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
+    return res.json({
+        speech: speech,
+        displayText: speech,
+        source: 'webhook-echo-sample'
+    });
+});  
 });
 
 
